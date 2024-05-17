16c8c1664c6b074b84e624540af274c179a25223
DENNISWUPC\ccdnd <dennis.wu@ccdntech.com>
2024-05-17 10:08:10 +0800

    fix.修正

--- app/Http/Controllers/WordPressController.php
+++ app/Http/Controllers/WordPressController.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Http;
 
 class WordPressController extends Controller
 {
-    // 方法：新增使用者
     public function createUser(Request $request)
     {
         $response = Http::withBasicAuth(env('WORDPRESS_USER'), env('WORDPRESS_PASSWORD'))
@@ -17,19 +16,17 @@ class WordPressController extends Controller
                             'email' => $request->email,
                             'first_name' => $request->first_name,
                             'last_name' => $request->last_name,
-                            'roles' => ['subscriber'] // 預設 訂閱者
+                            'roles' => ['subscriber']
                         ]);
 
         return $response->body();
     }
 
-    // 方法：修改使用者密碼
     public function updateUserPassword(Request $request)
     {
-        $username = $request->username; // 用户提交的用户名
-        $newPassword = $request->new_password; // 用户提交的新密码
+        $username = $request->username;
+        $newPassword = $request->new_password;
 
-        // 首先通过用户名查找用户ID
         $userResponse = Http::withBasicAuth(env('WORDPRESS_USER'), env('WORDPRESS_PASSWORD'))
                             ->get(env('WORDPRESS_URL') . '/wp-json/wp/v2/users', [
                                 'search' => $username
@@ -41,16 +38,13 @@ class WordPressController extends Controller
             return response()->json(['error' => "No user found with username: {$username}"], 404);
         }
 
-        // 假设 search 返回至少一个用户，并取第一个用户的 ID
         $userId = $users[0]['id'];
 
-        // 使用获得的用户 ID 更新密码
         $response = Http::withBasicAuth(env('WORDPRESS_USER'), env('WORDPRESS_PASSWORD'))
                         ->post(env('WORDPRESS_URL') . "/wp-json/wp/v2/users/{$userId}", [
                             'password' => $newPassword
                         ]);
 
-        // 返回 API 的响应
         return $response->body();
     }
 }
